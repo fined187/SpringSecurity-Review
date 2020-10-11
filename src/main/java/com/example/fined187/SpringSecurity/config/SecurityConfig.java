6ff2f293d7b1e67807df6904277a92ddc2e25454
fined000@naver.com <fined000@naver.com>
2020-10-11 20:55:49 +0900

    second commit

--- src/main/java/com/example/fined187/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/fined187/SpringSecurity/config/SecurityConfig.java
@@ -1,25 +1,32 @@
 package com.example.fined187.SpringSecurity.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
-@EnableWebSecurity                  //  config 사용 선언.
+@EnableWebSecurity
+@RequiredArgsConstructor
+//  config 사용 선언.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        인증
         http
                 .authorizeRequests()
-                .anyRequest()
+                .anyRequest()               //  어느 요청이 들어와도 인증이 되어야 함.
                 .authenticated();
 
 //        인증 방식.
         http
-                .formLogin()
+                .formLogin()               //   로그인을 통한 인증 방식.
 //                .loginPage("/loginPage")
 //                .defaultSuccessUrl("/defaultSuccessUrl")
                 .failureUrl("/login")
@@ -27,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordParameter("password")
                 .successHandler((AuthenticationSuccessHandler) (httpServletRequest, httpServletResponse, authentication) -> {
                     System.out.println("login success");
-                    httpServletResponse.sendRedirect("/defaultSuccessUrl");         //  defaultSuccessUrl로 리다이렉트
+                    httpServletResponse.sendRedirect("/");         //  defaultSuccessUrl로 리다이렉트
                 })
                 .failureHandler(((httpServletRequest, httpServletResponse, e) -> {
                     System.out.println("login fail");
@@ -44,5 +51,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         (httpServletRequest, httpServletResponse, authentication) ->
                                 System.out.println("logout saved")
                 );
+        http
+                .rememberMe()
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
+
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);           //  false default.
+
+//        http
+//                .sessionManagement()
+//                .sessionFixation().changeSessionId();
     }
 }
